3ed7fdd7ced2d254f653024fc4a121dcb69daf91
ishagupta <isha.gupta@menloschool.org>
2024-10-21 11:57:56 -0700

    Coding

--- src/DNA.java
+++ src/DNA.java
@@ -11,13 +11,34 @@
  */
 
 public class DNA {
+    // public static final long P = 54321102419;
+    public static final int R = 4;
+    static char[] letters = new char[84];
 
-    /**
-     * TODO: Complete this function, STRCount(), to return longest consecutive run of STR in sequence.
-     */
+
     public static int STRCount(String sequence, String STR) {
-        int[] map = new int[26];
+        letters['A'] = 'A';
+        letters['C'] = 'C';
+        letters['G'] = 'G';
+        letters['T'] = 'T';
+
+        int length = STR.length();
+        int strHash = hash(STR, length);
+        int hash = hash(sequence.substring(0, length-1), length);
+
+        for(int i = length; i < sequence.length(); i++){
+
+        }
 
         return 0;
+    }
+
+    // Creates has for the first time
+    public static int hash(String s, int length) {
+        int hash = 0;
+        for (int i = 0; i < length; i++) {
+            hash = (hash*R + letters[s.charAt(i)]);
+        }
+        return hash;
     }
 }
