fc29bf466c756f4e973b1f541226876b49a5e693
ishagupta <isha.gupta@menloschool.org>
2024-10-23 10:27:03 -0700

    Final

--- src/DNA.java
+++ src/DNA.java
@@ -11,10 +11,8 @@
  */
 
 public class DNA {
-    // public static final long P = 54321102419L;
     public static final int R = 4;
     static char[] letters = new char[85];
-
 
     public static int STRCount(String sequence, String STR) {
         // Insert all DNA letters into the letters map
@@ -30,21 +28,23 @@ public class DNA {
         int newSeqHash = 0;
         int numRepeats = 0;
         int maxRepeats = 0;
-        String testSeq = sequence.substring(0, length);
 
-        for(int i = length; i < sequence.length() - length; i++){
+        // Go through the whole DNA to check for repeats
+        int max = sequence.length() - length;
+        for(int i = length; i < max; i++){
             // If you find a match, check for consecutive matches
             if(strHash == seqHash){
-                // Shift over the hash
+                // Shift over the new hash so you can check the sequence in front of you
                 newSeqHash = hash(sequence, i, length);
                 numRepeats = 1;
 
-                // While ur still equal, continue to shift over by the length of the STR and add to repeats
+                // While you are still equal, continue to shift over by the length of the STR and add to repeats
                 while(strHash == newSeqHash){
+                    // Only update seqHash in the case where newSeqHash is a match
                     seqHash = hash(sequence, i, length);
                     numRepeats += 1;
-                    // If you can check the next sequence do so otherwise exit out
-                    if(i + length < sequence.length()) {
+                    // If you can check the next sequence w/o going out of bounds do so, otherwise exit out
+                    if(i + length < sequence.length()){
                         i += length;
                         newSeqHash = hash(sequence, i, length);
                     }
@@ -54,23 +54,22 @@ public class DNA {
                 // Updates maxRepeats if needed
                 if(numRepeats > maxRepeats)
                     maxRepeats = numRepeats;
-
             }
-            // Shift over by one(subtract first, x radix, add last)
-            // SeqHash - (value of first letter) * R^length-1
+
+            // Shift over the window by one letter
+            // First subtract out the first letter from hash
             int firstL = letters[sequence.charAt(i - length)];
-            //seqHash = (long)(seqHash + P - firstL * Math.pow(R, length-1)) % P;
-            seqHash = (seqHash - firstL * (1 << 2*length - 2));
-            // Multiply by radix and add value of last letter
+            seqHash = seqHash - firstL * (1 << 2*length - 2);
+            // Multiply by radix to shift over
             seqHash *= R;
+            // Add the value of the last letter
             seqHash += letters[sequence.charAt(i)];
 
         }
         return maxRepeats;
     }
 
-    // Creates has for the first time
-    // enter in length, index you start, and full sequence
+    // Creates hash for the first time and when you are scanning ahead
     public static int hash(String s, int start, int length) {
         int hash = 0;
         for (int i = 0; i < length; i++) {
